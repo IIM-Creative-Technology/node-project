aaec7d7909ff838fc2c729e04d4097af35c9057f
jstm17 <62430328+jstm17@users.noreply.github.com>
2022-01-05 12:26:18 +0100

    route login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).json({msg: "Hello world"});
+});
+
+// Login
+router.post('/login', function(req, res, next) {
+  console.log(req.body.username);
+  res.status(200).json({msg: req.body.username});
 });
 
 module.exports = router;
+
+
+
+
+
+
+// user
+// chatroom
+// Message
