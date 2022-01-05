76addccb4b1d4a5e7064a58861c778dce2f4094e
SGJipe <jeanpierre.han.206@gmail.com>
2022-01-05 11:56:29 +0100

    commit before pull of deletions and nodemon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).json({msg: "hello world"});
 });
+
+router.get('/login', function(req, res, next) {
+  res.status(200).json({msg: "connect"});
+});
+
 
 module.exports = router;
