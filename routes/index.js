d419e7363410c8aea5561ab12d7be2412eb560ab
Wintxer <47366828+Wintxer@users.noreply.github.com>
2022-01-05 14:26:41 +0100

    Mongoose installation

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).json({msg: 'Hello world'});
+});
+
+router.post('/login', function(req, res, next) {
+  console.log(req.body.username);
+  res.status(200).json({msg: req.body.username});
 });
 
 module.exports = router;
